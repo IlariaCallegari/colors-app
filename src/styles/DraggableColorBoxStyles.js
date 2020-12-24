39586c07787104059c902f12b0bd8d90982a829a
Ilaria Callegari <ilaria.callegari@gmail.com>
2020-12-24 16:35:39 +0000

    make form and navbar responsive

--- src/styles/DraggableColorBoxStyles.js
+++ src/styles/DraggableColorBoxStyles.js
@@ -1,3 +1,5 @@
+import sizes from "./Sizes";
+
 const styles = {
   root: {
     height: "25%",
@@ -11,8 +13,19 @@ const styles = {
       color: "white",
       transform: "scale(1.3)",
     },
+    [sizes.down("lg")]: {
+      width: "25%",
+      height: "20%",
+    },
+    [sizes.down("md")]: {
+      width: "50%",
+      height: "10%",
+    },
+    [sizes.down("sm")]: {
+      width: "100%",
+      height: "5%",
+    },
   },
-
   boxContent: {
     position: "absolute",
     display: "flex",
