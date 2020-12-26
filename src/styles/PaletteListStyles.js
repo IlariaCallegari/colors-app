e98739919bec3eb8d57e4620196cf52ae75eab2c
Ilaria Callegari <ilaria.callegari@gmail.com>
2020-12-26 14:38:05 +0000

    animate palette deletion

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,59 +1,69 @@
 import sizes from "./Sizes";
-import bg from "./bg.svg"
+import bg from "./bg.svg";
+import { purple } from "@material-ui/core/colors";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    root: {
-      display: "flex",
-      alignItems: "flex-start",
-      justifyContent: "center",
-      height: "100vh",
-      backgroundColor: "#124caa",
-      backgroundImage: `url(${bg})`,
-      /* background by SVGBackgrounds.com */
-      overflow: "scroll",
+  "@global": {
+    ".fade-exit": {
+      opacity: 1,
     },
-    container: {
-      width: "50%",
-      display: "flex",
-      alignItems: "flex-start",
-      flexDirection: "column",
-      flexWrap: "wrap",
-      [sizes.down("xl")]: {
-        width: "80%",
-      },
-      [sizes.down("sm")]: {
-        width: "70%",
-      },
-      [sizes.down("xs")]: {
-        width: "60%",
-      },
-    },
-    nav: {
-      display: "flex",
-      width: "100%",
-      justifyContent: "space-between",
-      alignItems: "center",
-      color: "white",
-      "& a": {
-        color: "white",
-        textDecoration: "none"
-      }
-    },
-    palettes:{
-      boxSizing: "border-box",
-      width: "100%",
-      display: "grid", 
-      gridTemplateColumns: "repeat(3, 30%)",
-      gridGap: "2.5rem",
-      [sizes.down("sm")]: {
-        gridTemplateColumns: "repeat(2, 50%)",
-        gap: "1rem"
-      },
-      [sizes.down("xs")]: {
-        gridTemplateColumns: "repeat(1, 100%)",
-      },
-    },
-    heading: {
-      fontSize: "2rem"
+    ".fade-exit-active": {
+      opacity: 0,
+      transition: "opacity 0.5s ease-out",
     }
-  };
+  },
+  root: {
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    height: "100vh",
+    backgroundColor: "#124caa",
+    backgroundImage: `url(${bg})`,
+    /* background by SVGBackgrounds.com */
+    overflow: "scroll",
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "80%",
+    },
+    [sizes.down("sm")]: {
+      width: "70%",
+    },
+    [sizes.down("xs")]: {
+      width: "60%",
+    },
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    color: "white",
+    "& a": {
+      color: "white",
+      textDecoration: "none",
+    },
+  },
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "2.5rem",
+    [sizes.down("sm")]: {
+      gridTemplateColumns: "repeat(2, 50%)",
+      gap: "1rem",
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1, 100%)",
+    },
+  },
+  heading: {
+    fontSize: "2rem",
+  },
+};
